c1250accacf4ccbc1b7a934cd770813b7157599d
jorgeAP <jorgeagoiz@gmail.com>
2021-03-28 20:50:25 +0200

    Empezando con el borrador del ejercicio

---
+++ index.js
@@ -0,0 +1,55 @@
+const containerOne = document.querySelector(".container");
+
+const mainMenu = () => {
+  // Title
+  const menu = document.createElement("div");
+  menu.classList.add("menu");
+  menu.innerHTML = "<h1>Menu</h1>";
+
+  // Select players
+  const selectPlayers = document.createElement("div");
+  selectPlayers.innerHTML = `
+                            <h2>Selecciona el numero de corredores:</h2>
+                                <select class="selector" name="combo">
+                                    <!-- Opciones de la lista -->
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                </select>
+                            `;
+
+  const numPlayers = document.querySelector(".selector");
+  const sendNumber = document.createElement("button");
+  sendNumber.innerText = "Jugar";
+  sendNumber.classList.add("btn-send");
+  sendNumber.addEventListener("click", (event) => {
+    const num = event.path[1].childNodes[1].childNodes[3].value;
+    menu.remove();
+    startRace(num);
+    // Aqui le pasamos la funcion de comenzar carrera
+  });
+  menu.appendChild(selectPlayers);
+  menu.appendChild(sendNumber);
+  containerOne.appendChild(menu);
+};
+
+const startRace = (players) => {
+  console.log(players);
+  let playersArray = [];
+  for (let x = 0; x < players; x++) {
+    let position = document.createElement("div");
+    let car = document.createElement("img");
+    car.src = `./img/car${x + 1}.png`;
+    position.appendChild(car);
+    containerOne.appendChild(position);
+    //Nos quedamos aqui para añadir estilos a cada coche
+    //Añadirlos a un array
+  }
+};
+
+mainMenu();
